77550b82ab8c1653ffb04a08a937c159b292b877
Matias Castellanos <a2024112625@estudiantes.upsa.edu.bo>
2025-09-29 12:58:46 -0400

    Se agrego la tabla LaPaz

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ComidasTipicasAPI.Models;
 namespace ComidasTipicasAPI.Data;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<LaPaz> LaPazes { get; set; }
 }
