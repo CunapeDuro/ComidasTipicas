804976a9bd02b51b6c0be280044b3848383e1256
Victoria Frias Muñoz <a2024112803@estudiantes.upsa.edu.bo>
2025-09-29 13:00:34 -0400

    Revisando

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ComidasTipicasAPI.Models;
 namespace ComidasTipicasAPI.Data;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<SantaCruz> SantaCruces { get; set; }
 }
--- Models/SantaCruz.cs
+++ Models/SantaCruz.cs
@@ -1,6 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ComidasTipicasAPI.Models;
 
 public class SantaCruz
 {
-    
+    [Key]
+    public int Id { get; set; }
+
+    [Required, StringLength(80)]
+    public string Nombre { get; set; } = null!;                  // ej. "Majadito"
+
+    [Required, StringLength(120)]
+    public string IngredientePrincipal { get; set; } = null!;    // ej. "Charque de res"
+
+    [Required, StringLength(120)]
+    public string Restaurante { get; set; } = null!;             // ej. "Fogón Cruceño"
+
+    [Range(0, 1000)]
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal Precio { get; set; }                           // ej. 45.00
 }
