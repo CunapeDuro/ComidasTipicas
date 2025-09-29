13e90c14970e84a1c1ece27f9a54eb0b45ccb17d
Victoria Frias Muñoz <a2024112803@estudiantes.upsa.edu.bo>
2025-09-29 13:03:56 -0400

    migracionrealizada

---
+++ Migrations/20250929170235_AddSantaCruzTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComidasTipicasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSantaCruzTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SantaCruces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    IngredientePrincipal = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Restaurante = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SantaCruces", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SantaCruces");
+        }
+    }
+}
